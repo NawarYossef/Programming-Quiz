f426a74fbdf7fef5a62d72d7c99ebb644680c483
Nawar <nawaryossef2@gmail.com>
2017-10-20 02:28:41 -0400

    create data object that hold all quiz data (questions and answers).

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -3,19 +3,21 @@
 "use strict";
 
 
-let obj = {
-	message: function(){
-		$(document).ready(function(){
-	console.log($("h1").text())
-		
-	})
-	},
-
-	bad: function(){}
+let data = {
+	questions: {
+		1: {
+			question: 'Which of the following solutions cna be used to convert the string "1234" to an array of numbers [1, 2, 3, 4]:',
+			answers: ['String("1234").Array((char) => char.toNumber())', 'Array.from("1234").map(Number)', '"1234".map(Number).toArray()', '"1234".split("").Array().Number()']
+		},
+		2: {
+			question: 'What will the following code log to the console:',
+			answers: []
+		}
+	}
 }
 
 
-module.exports = obj;
+module.exports = javascriptFundementals
 
 },{}],2:[function(require,module,exports){
 /* global $ */
