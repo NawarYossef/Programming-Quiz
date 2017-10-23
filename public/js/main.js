eeb0cdc4632a214bbd5166c0df14a6f552bb4455
Nawar <nawaryossef2@gmail.com>
2017-10-23 17:32:57 -0400

    add final result page. display user score

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 /* global $ */
 "use strict";
-let javascriptFundementals = require('./js-fundementals-quiz.js');
-let advancedJavascript = require('./js-advanced.js');
-let _ = require('lodash');
+const javascriptFundementals = require('./js-fundementals-quiz.js');
+const advancedJavascript = require('./js-advanced.js');
+const _ = require('lodash');
 
 class QuizApp {
 	constructor() {
@@ -23,17 +23,22 @@ class QuizApp {
 	}
 
 	hideQuiz() {
-		$(".questions, .correct-ans, .wrong-ans, .count").hide();
+		$(".questions, .correct-ans, .wrong-ans, .count, .final-score, .results").hide();
 	}
 
 	renderQuizMarkup() {
-		$(function(){
-			$(".quiz-btn").click(function() {
-				$("main").hide()
-				$(".questions, .count").fadeIn(200)
-				
-			})
+		$(".quiz-btn").click(function() {
+			$("main").hide()
+			$(".questions, .count").fadeIn(200)	
 		})
+	}
+
+	renderMainPageMarkUp() {
+		$("main").fadeIn(200).show()
+	}
+
+	mainPageStyleRender() {
+		$('link[href="stylesheets/style2.css"]').attr({'href' : "stylesheets/style1.css"})
 	}
 
 	quizStyleRender() {
@@ -44,97 +49,154 @@ class QuizApp {
 	
 	questionCount() {
 		let range = Object.keys(this.state).length;
-		$(".question-count").text(`${this.index}/${range}`)
-	}
-	// shuffleQuestions() {
+		$(".question-count").text(`${this.index}/${range}`);
 		
-	// }
-
+		//hide next question if it's the last question on the quiz
+		this.hideNextQuesButton();
+	}
+	
 
 	displayFirstQuestion() {
-		let that = this
-		$(".quiz-btn").click(function() {
-			$("h2").text(that.state[that.index]["question"])
+		$(".quiz-btn").click(() => {
+			// show question count
+			this.questionCount();
 
+			$("h3").text(this.state[this.index]["question"])
+
+			let context = this;
 			$(".answer").each(function(idx, ele) {
-				$(this).text(that.state[that.index]["answers"][idx])
+				$(this).text(context.state[context.index]["answers"][idx])
 			})
 		})
 	}
 
 	getUserChoiceAndShowResult() {
-		let that = this;
-		$(".option").click(function() {
-	
+		$(".option").click((e) => {
 			// collect user choice
-			that.userChoiceIdx = $(this).find("input").attr("id")
+			this.userChoiceIdx = $(e.currentTarget).find("input").attr("id")
 
 			// hide multiple choices in order to display result only
-			that.formHide();
+			this.formHide();
 
 			// show result based on user choice collected
-			that.showResult()
+			this.showResult();
+
+			//allow user to go to next question
+			this.enableNextButton();
 		})
 		
 	}
 
 	// if function returns true, then user made the right choice. 
 	userMadeRightChoice() {
-		return Number(this.userChoiceIdx) === this.state[this.index]["correctAnswer"]
+		return Number(this.userChoiceIdx) === this.state[this.index]["correctAnswer"];
 	}
 
 	formHide() {
-		$("form").fadeOut(500).hide()	
+		$("form").fadeOut(500).hide();
 	}
 
 	showForm() {
-		$("form").fadeIn(300).show()
+		$("form").fadeIn(300).show();
+	}
+
+	hideQuestion() {
+		$("h3").hide();
+	}
+
+	showQuestion() {
+		$("h3").show()
+	}
+	wrongAnsText() {
+		let pos = this.state[this.index]["correctAnswer"] - 1
+		let text = $(`<p class="corr-ans-display">${this.state[this.index]["answers"][pos]}</p>`)
+		$(".wrong-ans").append(text);
 	}
 
 	showResult() {
 		if (this.userMadeRightChoice()) {
 			$(".correct-ans").fadeIn(200)
+			// hide question
+			this.hideQuestion()
 		} else if (this.userMadeRightChoice() === false) {
+			this.wrongAnsText()
 			$(".wrong-ans").fadeIn(200)
 		}
+
+		this.renderResultsButton()
 	}
 
-	hideResult() {
-		$(".correct-ans").hide()
-		$(".wrong-ans").hide()
+	hideStats() {
+		$(".correct-ans").hide();
+		$(".wrong-ans").hide();
+		$(".corr-ans-display").remove();
 	}
 
 	enableNextButton() {
-		$(".option").click(function() {
-			$(".next-q").prop('disabled', false);
-		})
-		
+		$(".next-q").prop('disabled', false);
 	}
 
 	disableNextButton() {
-		$(".next-q" ).click(function() {
-			$(this).prop('disabled', true);
+		$(".next-q").prop('disabled', true);
+	}
+	
+	updateStateForNextQuestion() {
+		this.showQuestion();
+		this.questionCount();
+		this.hideStats();
+		this.showForm();
+		this.disableNextButton();
+	}
+
+	renderNextQuestion() {
+		$(".next-q" ).click(() => {
+			this.index += 1;
+			this.updateStateForNextQuestion()
+
+			$("h3").text(this.state[this.index]["question"]);
+
+			let that = this;
+			$(".answer").each(function(idx, ele) {
+				$(this).text(that.state[that.index]["answers"][idx]);
+			})
+		})	
+	}
+	
+	//hide next question if it's the last question on the quiz
+	hideNextQuesButton() {
+		let range = Object.keys(this.state).length
+		this.index === range ? $(".next-q").hide() : null;
+	}
+	
+	mainPageHeaderShow() {
+		$("h2").show();
+	}
+
+	emptyQuestion() {
+		$("h3").empty();
+	}
+	
+	backToMainDefaultState() {
+		$(".back-to-main").click(() => {
+			this.state = '';
+			this.userChoiceIdx = '';
+			this.index = 1;
+			this.hideQuiz();
+			this.mainPageStyleRender();
+			this.renderMainPageMarkUp();
+			this.mainPageHeaderShow();
+			this.emptyQuestion();
 		})
 	}
 	
-	renderNextQuestion() {
-		let that = this;
-		$(".next-q" ).click(function() {
-			that.index += 1;
-			// render current index for question
-			that.questionCount()
 
-			that.hideResult()
-			that.showForm()
+	renderResultsButton() {
+		let range = Object.keys(this.state).length
+		this.index === range ? $(".results").show() : null;
+	}
 
-			$("h2").text(that.state[that.index]["question"])
-
-			$(".answer").each(function(idx, ele) {
-				$(this).text(that.state[that.index]["answers"][idx])
-			})
-		})	
-	}	
-
+	// shuffle questions
+	// show final result
 	init() {
 		this.hideQuiz();
 		this.whichQuiz();
@@ -143,9 +205,8 @@ class QuizApp {
 		this.questionCount();
 		this.displayFirstQuestion();
 		this.getUserChoiceAndShowResult();
-		this.enableNextButton();
 		this.renderNextQuestion();
-		this.disableNextButton()
+		this.backToMainDefaultState();
 	}
 }
 
